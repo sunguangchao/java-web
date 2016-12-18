b1524839cd1b0c43603c00c893c657f63db3e7d9
sunguangchao <1198125767@qq.com>
2016-12-18 23:48:18 +0800

    shopping management system realize by me
    Signed-off-by: sunguangchao <1198125767@qq.com>

---
+++ shopping system00/src/db/DbClose.java
@@ -0,0 +1,72 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by 11981 on 2016/12/10.
+ */
+public final class DbClose {
+    /**
+     * 关闭 添加功能 资源
+     * @param pstmt,rs,conn
+     */
+    public static void addClose(PreparedStatement pstmt, Connection conn)
+    {
+        /*
+	    * 多个 try-catch 出发点：安全
+		*/
+        try{
+            if(pstmt != null)
+            {
+                pstmt.close();
+            }
+        }catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        try{
+            if(conn != null)
+            {
+                conn.close();
+            }
+        }catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * 关闭资源
+     * @param pstmt,rs,conn
+     */
+
+    public static void queryClose(PreparedStatement pstmt, ResultSet rs, Connection conn)
+    {
+        try{
+            if(pstmt != null)
+            {
+                pstmt.close();
+            }
+        }catch (SQLException e1)
+        {
+            e1.printStackTrace();
+        }
+        try{
+            if(rs != null)
+            {
+                rs.close();
+            }
+        }catch (SQLException e1){
+            e1.printStackTrace();
+        }
+        try{
+            if(conn != null){
+                conn.close();
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ shopping system00/src/tools/Arith.java
@@ -0,0 +1,78 @@
+package tools;
+
+import java.math.BigDecimal;
+
+/**由于Java的简单类型不能够精确的对浮点数进行运算，这个工具类提供精
+ * 确的浮点数运算，包括加减乘除和四舍五入.
+ * Created by 11981 on 2016/12/10.
+ */
+public class Arith {
+
+    private static final int DEF_DIV_SCALE = 2;//默认除法运算精度
+    private Arith(){}
+    /**
+     * 提供精确的加法运算。
+     * @param v1 被加数
+     * @param v2 加数
+     * @return 两个参数的和
+     */
+    public static double add(double v1, double v2)
+    {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.add(b2).doubleValue();
+    }
+    public static double sub(double v1, double v2)
+    {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.subtract(b2).doubleValue();
+    }
+    public static double mul(double v1,double v2)
+    {
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.multiply(b2).doubleValue();
+    }
+    /**
+     * 提供（相对）精确的除法运算，当发生除不尽的情况时，精确到
+     * 小数点以后2位，以后的数字四舍五入。
+     * @param v1 被除数
+     * @param v2 除数
+     * @return 两个参数的商
+     */
+    public static double div(double v1,double v2)
+    {
+        return div(v1,v2,DEF_DIV_SCALE);
+    }
+
+    /**
+     * 提供（相对）精确的除法运算。当发生除不尽的情况时，由scale参数指
+     * 定精度，以后的数字四舍五入。
+     * @param v1 被除数
+     * @param v2 除数
+     * @param scale 表示表示需要精确到小数点以后几位。
+     * @return 两个参数的商
+     */
+    public static double div(double v1, double v2, int scale)
+    {
+        if(scale < 0)
+        {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(Double.toString(v1));
+        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();//遇到.5的情况向上近似
+    }
+    public static double round(double v,int scale)
+    {
+        if(scale<0)
+        {
+            throw new IllegalArgumentException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b = new BigDecimal(Double.toString(v));
+        BigDecimal one = new BigDecimal("1");
+        return b.divide(one,scale,BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+
+}
