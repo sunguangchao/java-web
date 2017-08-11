7d6cb19eb9fa52dca007c8363612b8271bb9f37d
sunguangchao <1198125767@qq.com>
2017-08-11 14:38:17 +0800

    customer-support project start -Add v1 and v2
    Signed-off-by: sunguangchao <1198125767@qq.com>

---
+++ customer-support/Customer-Support-v1/src/main/java/Attachment.java
@@ -0,0 +1,27 @@
+/**
+ * Created by 11981 on 2017/8/9.
+ */
+public class Attachment {
+    private String name;
+    private byte[] contents;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getContents() {
+        return contents;
+    }
+
+    public void setContents(byte[] contents) {
+        this.contents = contents;
+    }
+
+
+
+}
